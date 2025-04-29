91fdcc39e65892da1ae62d86f427355ca9067adf
kkwatts <kkwatts15@gmail.com>
2025-04-29 17:52:14 -0500

    Added attacking
    The player can use the space bar to attack the crab and the crab will slowly attack over time

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,14 +6,27 @@ public class HealthManager : MonoBehaviour {
     public Slider healthBar;
 
     private float health;
+    private float attackTimer;
+    private float attackTimerThreshold;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         health = maxHealth;
+        attackTimerThreshold = 2f;
+        attackTimer = attackTimerThreshold;
     }
 
     // Update is called once per frame
     void Update() {
         healthBar.value = health / maxHealth;
+
+        attackTimer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && gameObject.CompareTag("Enemy")) {
+            health--;
+        }
+        else if (attackTimer < 0f && gameObject.CompareTag("Player")) {
+            health--;
+            attackTimer = attackTimerThreshold;
+        }
     }
 }
